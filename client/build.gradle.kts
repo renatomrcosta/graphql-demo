4d5ae836ff800454ea76e4a12847e3c426c3593b
Renato Costa <renato.rezende.costa@gmail.com>
2021-09-21 21:28:38 +0200

    sample client impl

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.spring") version "1.5.21"
+    id("com.expediagroup.graphql") version "4.2.0"
 }
 
 group = "com.xunfos.graphqldemo"
@@ -21,7 +22,18 @@ repositories {
     mavenCentral()
 }
 
+graphql {
+    client {
+        endpoint = "http://localhost:8080/graphql"
+        packageName = "com.xunfos.generated"
+    }
+}
+
 dependencies {
+    implementation("com.expediagroup:graphql-kotlin-spring-client:4.2.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
--- client/src/main/kotlin/com/xunfos/graphqldemo/client/ClientApplication.kt
+++ client/src/main/kotlin/com/xunfos/graphqldemo/client/ClientApplication.kt
@@ -1,11 +1,38 @@
 package com.xunfos.graphqldemo.client
 
+import com.expediagroup.graphql.client.spring.GraphQLWebClient
+import com.xunfos.generated.ArtistQuery
+import com.xunfos.generated.artistquery.Artist
+import kotlinx.coroutines.coroutineScope
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class ClientApplication
 
 fun main(args: Array<String>) {
     runApplication<ClientApplication>(*args)
+}
+
+@RestController
+class MyQueryController(
+    private val graphQLWebClient: GraphQLWebClient,
+) {
+    @GetMapping("/nanas")
+    suspend fun nanas(): List<Artist> = coroutineScope {
+        val query = ArtistQuery()
+        val result = graphQLWebClient.execute(query)
+        result.data?.artists ?: emptyList()
+    }
+}
+
+@Configuration
+class Configs {
+    @Bean
+    fun graphqlClient(): GraphQLWebClient = GraphQLWebClient(url = "http://localhost:8080/graphql")
 }

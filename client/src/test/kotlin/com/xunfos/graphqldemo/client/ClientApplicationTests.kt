30d5dad720408e438452f32a07634d098f41c3a2
Renato Costa <renato.rezende.costa@gmail.com>
2021-09-21 21:06:18 +0200

    add prototypes for client and server modules

---
+++ client/src/test/kotlin/com/xunfos/graphqldemo/client/ClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xunfos.graphqldemo.client
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
